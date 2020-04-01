5a16d3aeea473d58526254870e9f00bc272f8b25
ruthetum <ruthetum@gmail.com>
2020-04-02 05:11:59 +0900

    전화 화면으로 넘기기(intent 활용)

---
+++ basic-boostcourse/MyCallIntent/app/src/main/java/com/example/callintent/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.callintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String receiver = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + receiver));
+                startActivity(intent);
+            }
+        });
+    }
+
+}
