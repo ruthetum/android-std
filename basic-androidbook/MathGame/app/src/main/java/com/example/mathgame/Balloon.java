8f9e7ba3571cc7a39a222d47ca60011b5db2453f
ruthetum <ruthetum@gmail.com>
2020-04-06 23:59:32 +0900

    브로드캐스트 수신자 이용하기

---
+++ basic-androidbook/MathGame/app/src/main/java/com/example/mathgame/Balloon.java
@@ -0,0 +1,4 @@
+package com.example.mathgame;
+
+public class Balloon {
+}
---
+++ basic-boostcourse/MySMSReceiver/app/src/main/java/com/example/receiver/SmsReceiver.java
@@ -0,0 +1,15 @@
+package com.example.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "SmsReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive() 호출됨");
+    }
+}
