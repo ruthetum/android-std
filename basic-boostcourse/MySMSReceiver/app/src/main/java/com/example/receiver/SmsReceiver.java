3d02a391eccf8349da9a611bc3058a7ac928275d
ruthetum <ruthetum@gmail.com>
2020-04-07 00:47:22 +0900

    브로드캐스트 수신자 이용하기(화면에 SMS 내용 띄우기)

--- basic-boostcourse/MySMSReceiver/app/src/main/java/com/example/receiver/SmsReceiver.java
+++ basic-boostcourse/MySMSReceiver/app/src/main/java/com/example/receiver/SmsReceiver.java
@@ -8,11 +8,14 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SmsReceiver extends BroadcastReceiver {
 
     private static final String TAG = "SmsReceiver";
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive() 호출됨");
@@ -29,14 +32,29 @@ public class SmsReceiver extends BroadcastReceiver {
 
             Date receivedDate = new Date(messages[0].getTimestampMillis());
             Log.d(TAG, "received date : "+receivedDate);
+
+            sendToAcitivity(context, sender, contents, receivedDate);
         }
+    }
+
+    private void sendToAcitivity(Context context, String sender, String contents, Date receivedDate) {
+        Intent intent = new Intent(context, SmsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|
+                        Intent.FLAG_ACTIVITY_SINGLE_TOP|
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        intent.putExtra("sender", sender);
+        intent.putExtra("contents", contents);
+        intent.putExtra("receivedDate", format.format(receivedDate));
+
+        context.startActivity(intent);
     }
 
     private SmsMessage[] parseSmsMessage(Bundle bundle) {
         Object[] objs = (Object[]) bundle.get("pdus"); // pdus : sms 관련 데이터 정보가 들어가 있음
         SmsMessage[] messages = new SmsMessage[objs.length];
 
-        for (int i=0l i < objs.length; i++) {
+        for (int i=0 ; i < objs.length; i++) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 String format = bundle.getString("format");
                 messages[i] = SmsMessage.createFromPdu((byte[])objs[i], format);
