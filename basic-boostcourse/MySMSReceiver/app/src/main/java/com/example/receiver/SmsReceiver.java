e03d8c0f6136eed7cc7cd97d8fe710b5e0c6f26d
ruthetum <ruthetum@gmail.com>
2020-04-07 00:18:30 +0900

    브로드캐스트 수신자 이용하기(추가)

--- basic-boostcourse/MySMSReceiver/app/src/main/java/com/example/receiver/SmsReceiver.java
+++ basic-boostcourse/MySMSReceiver/app/src/main/java/com/example/receiver/SmsReceiver.java
@@ -3,7 +3,12 @@ package com.example.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
 import android.util.Log;
+
+import java.util.Date;
 
 public class SmsReceiver extends BroadcastReceiver {
 
@@ -11,5 +16,34 @@ public class SmsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive() 호출됨");
+
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] messages = parseSmsMessage(bundle);
+
+        if (messages.length > 0) {
+            String sender = messages[0].getOriginatingAddress();
+            Log.d(TAG, "sender : "+sender);
+
+            String contents = messages[0].getMessageBody().toString();
+            Log.d(TAG, "contents : "+contents);
+
+            Date receivedDate = new Date(messages[0].getTimestampMillis());
+            Log.d(TAG, "received date : "+receivedDate);
+        }
+    }
+
+    private SmsMessage[] parseSmsMessage(Bundle bundle) {
+        Object[] objs = (Object[]) bundle.get("pdus"); // pdus : sms 관련 데이터 정보가 들어가 있음
+        SmsMessage[] messages = new SmsMessage[objs.length];
+
+        for (int i=0l i < objs.length; i++) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                String format = bundle.getString("format");
+                messages[i] = SmsMessage.createFromPdu((byte[])objs[i], format);
+            } else {
+                messages[i] = SmsMessage.createFromPdu((byte[])objs[i]);
+            }
+        }
+        return messages;
     }
 }
