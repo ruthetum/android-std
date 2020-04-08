786abac0720b9683c0deddadc7ac671a8d1f01da
ruthetum <ruthetum@gmail.com>
2020-04-08 23:55:14 +0900

    프래그먼트 2개 이용해서 화면 전환하기

--- basic-boostcourse/MyFragment/app/src/main/java/com/example/fragment/MainActivity.java
+++ basic-boostcourse/MyFragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainFragment fragment1;
+    MenuFragment fragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment1 = new MainFragment();
+        fragment2 = new MenuFragment();
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 트랜잭션 안에서 수행되기 때문에 commit을 꼭 해줘야 됨
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 트랜잭션 안에서 수행되기 때문에 commit을 꼭 해줘야 됨
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+            }
+        });
     }
 }
