7a502d28ed2fd1482d9331e91a82f4eef713752a
ruthetum <ruthetum@gmail.com>
2020-04-09 00:58:06 +0900

    프래그먼트 간 화면 전환시키기

--- basic-boostcourse/MyFragment/app/src/main/java/com/example/fragment/MainActivity.java
+++ basic-boostcourse/MyFragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -38,4 +38,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void onFragmentChange(int index) {
+        if (index == 0) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+        } else if (index == 1) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+        }
+    }
 }
