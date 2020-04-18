930f82895527ad4b38e353bad7ea214dfa068b83
ruthetum <ruthetum@gmail.com>
2020-04-19 01:04:01 +0900

    옵션바 버튼 클릭 시 토트스 메시지 띄우기

--- basic-boostcourse/MyOptionMenu/app/src/main/java/com/example/optionmenu/MainActivity.java
+++ basic-boostcourse/MyOptionMenu/app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.optionmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +22,26 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int curId = item.getItemId();
+        switch(curId){
+            case R.id.menu_refresh:
+                Toast.makeText(this, "새로 고침 메뉴 클릭", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_search:
+                Toast.makeText(this, "검색 메뉴 클릭", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_settings:
+                Toast.makeText(this, "설정 메뉴 클릭", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
