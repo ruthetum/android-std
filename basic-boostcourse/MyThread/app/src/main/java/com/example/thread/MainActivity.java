dfd2d3dd506d1fc5257186d599c5d0c384c9a895
ruthetum <ruthetum@gmail.com>
2020-04-27 04:26:41 +0900

    쓰레드 이용

---
+++ basic-boostcourse/MyThread/app/src/main/java/com/example/thread/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.thread;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+//    int value = 0;
+
+    ValueHandler handler = new ValueHandler();
+
+    Handler handler2 = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                BackgroundThread thread = new BackgroundThread();
+//                thread.start();
+
+                new Thread(new Runnable(){
+                    int value = 0;
+                    boolean running = false;
+                   public void run(){
+                       running = true;
+                       while(running) {
+                           value += 1;
+
+                           handler2.post(new Runnable() {
+                               public void run(){
+                                   textView.setText("현재 값 : "+value);
+                               }
+                           });
+
+                           try{
+                               Thread.sleep(1000);
+                           } catch(Exception e) {}
+                       }
+                   }
+                }).start();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                textView.setText("현재 값 : "+value);
+            }
+        });
+    }
+
+    class BackgroundThread extends Thread{
+        int value = 0;
+        boolean running = false;
+        public void run() {
+            running = true;
+            while(running) {
+                value += 1;
+
+                Message message = handler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("value", value);
+                message.setData(bundle);
+                handler.sendMessage(message);
+
+                try{
+                    Thread.sleep(1000);
+                } catch(Exception e) {}
+            }
+        }
+    }
+
+    class ValueHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            Bundle bundle = msg.getData();
+            int value = bundle.getInt("value");
+            textView.setText("현재 값 : "+value);
+        }
+    }
+}
