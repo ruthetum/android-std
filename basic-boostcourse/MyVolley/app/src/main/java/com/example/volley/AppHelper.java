952c6ced53c0f689237ef237a2b0f668d5ffc54a
ruthetum <ruthetum@gmail.com>
2020-04-30 08:46:12 +0900

    핸들러 없이 UI 접근(Volley 라이브러리 사용)

---
+++ basic-boostcourse/MyVolley/app/src/main/java/com/example/volley/AppHelper.java
@@ -0,0 +1,8 @@
+package com.example.volley;
+
+import com.android.volley.RequestQueue;
+
+public class AppHelper {
+    public static RequestQueue requestQueue;
+
+}
---
+++ basic-boostcourse/MyVolley/app/src/main/java/com/example/volley/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.volley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        Button button =(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // volley는 외부 라이브러리 - grade에서 dependency에서 추가
+                sendRequest();
+            }
+        });
+
+        if (AppHelper.requestQueue == null) {
+            AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+    }
+
+    public void sendRequest() {
+        String url = "https://www.google.co.kr";
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        println("응답 -> " + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        println("에러 -> " + error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                return params;
+            }
+        };
+
+        // 이전 결과 있더라도 새로 요청해서 응답 보여줌
+        request.setShouldCache(false);
+
+        AppHelper.requestQueue.add(request);
+
+        println("요청 보냄.");
+    }
+
+    public void println(String data) {
+        textView.append(data + "\n");
+    }
+}
