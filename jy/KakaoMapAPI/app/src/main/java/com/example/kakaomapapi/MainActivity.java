9b4b66e16481e1e0489c9bbef0ae212097d31ebb
Heedong Kim <ruthetum@gmail.com>
2020-09-14 00:48:29 +0900

    카카오맵 연동하기

---
+++ jy/KakaoMapAPI/app/src/main/java/com/example/kakaomapapi/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.kakaomapapi;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.daum.mf.map.api.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+    }
+}
